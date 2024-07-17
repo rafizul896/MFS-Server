e875373662d8daf6815954bcaae69310e7058560
rafizul896 <rafizulislam896@gmail.com>
2024-07-17 12:16:39 +0600

    add: login and register

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 const app = express();
+const bcryct = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
+
 const port = process.env.PORT || 5000;
 
 // middleware
@@ -27,11 +31,56 @@ const client = new MongoClient(uri, {
 
 async function run() {
     try {
-        const usersCollection = client.db('mfs').collection('user');
+        const usersCollection = client.db('mfs').collection('users');
 
         app.get('/users', async (req, res) => {
             const result = await usersCollection.find().toArray();
             res.send(result);
+        })
+
+        app.get('/user/:id',async(req,res)=>{
+            const id = req.params.id;
+            const query = {_id: new ObjectId(id)};
+            const result = await usersCollection.findOne(query);
+            res.send(result);
+        })
+        // register a user
+        app.post('/register', async (req, res) => {
+            const { name, mobileNumber, email, pin } = req.body;
+            const hashedPin = await bcryct.hash(pin, 10);
+            const user = {
+                name,
+                pin: hashedPin,
+                mobileNumber,
+                email,
+                status: 'pending',
+                role: 'User',
+                balance: 0
+            }
+            const exist = await usersCollection.findOne({ $or: [{ mobileNumber: mobileNumber }, { email: email }] });
+            if (exist) return res.send({ message: 'Already as a Account' })
+            const result = await usersCollection.insertOne(user);
+            res.send(result);
+        })
+        // login a user
+        app.post('/login', async (req, res) => {
+            const { identifier, pin } = req.body;
+            const user = await usersCollection.findOne({ $or: [{ mobileNumber: identifier }, { email: identifier }] });
+            if (user && await bcryct.compare(pin, user.pin)) {
+                const token = jwt.sign(user, process.env.ACCESS_TOKEN_KEY, { expiresIn: '1d' });
+                res.cookie('token', token, {
+                    httpOnly: true,
+                    secure: process.env.NODE_ENV === 'production',
+                    sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict'
+                }).send({ id: user._id })
+            } else {
+                res.status(400).json({ message: 'Invalid credentials' });
+            }
+        })
+        // jwt token clear cookie
+        app.post('/logout', async (req, res) => {
+            const user = req.body;
+            res.clearCookie('token', { maxAge: 0 }).send({ success: true })
         })
 
         // console.log("Pinged your deployment. You successfully connected to MongoDB!");
